a20f932b6fb89f18c3db41d86db3cf6c320c7a74
Dimitris <admin@wiseinno.tech>
2017-08-31 18:07:35 +0300

    added sunrise and sunset times on the gem

--- lib/openweather2/weather.rb
+++ lib/openweather2/weather.rb
@@ -2,7 +2,8 @@ module Openweather2
   class Weather
 
     attr_reader :city, :longitude, :latitude, :pressure, :humidity, :clouds,
-      :temperature, :min_temperature, :max_temperature, :wind_speed, :wind_angle
+                :temperature, :min_temperature, :max_temperature, :wind_speed,
+                :wind_angle, :sunrise, :sunset
 
     def initialize(json)
       @city = json['name']
@@ -16,6 +17,8 @@ module Openweather2
       @clouds = json['clouds']['all']
       @wind_speed = json['wind']['speed']
       @wind_angle = json['wind']['deg']
+      @sunrise =  json['sys']['sunrise']
+      @sunset = json['sys']['sunset']
     end
   end
 end
